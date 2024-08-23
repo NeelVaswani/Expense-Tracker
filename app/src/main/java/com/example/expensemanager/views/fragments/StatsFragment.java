63343441bc6634471b5ef3a80b5ef7f3c441df7a
neelv <neelvaswani5@gmail.com>
2024-08-22 22:53:46 -0700

    App Version 2

--- app/src/main/java/com/example/expensemanager/views/fragments/StatsFragment.java
+++ app/src/main/java/com/example/expensemanager/views/fragments/StatsFragment.java
@@ -38,139 +38,172 @@ import io.realm.RealmResults;
 public class StatsFragment extends Fragment {
 
 
-
-    public StatsFragment() {
-        // Required empty public constructor
-    }
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-    FragmentStatsBinding binding;
-    public MainViewModel viewModel;
-    Calendar calendar;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        binding = FragmentStatsBinding.inflate(inflater);
-
-        viewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
-        calendar = Calendar.getInstance();
-        updateDate();
-
-        binding.nextDate.setOnClickListener(c-> {
-
-            if (Constants.SELECTED_TAB == 0) {
-                calendar.add(Calendar.DATE, 1);
-            }
-            else if (Constants.SELECTED_TAB == 1) {
-                calendar.add(Calendar.MONTH, 1);
-            }
-
-            updateDate();
-
-        });
-
-        binding.prevDate.setOnClickListener(c-> {
-            if (Constants.SELECTED_TAB_STATS == 0) {
-                calendar.add(Calendar.DATE, -1);
-            }
-            else if (Constants.SELECTED_TAB_STATS == 1) {
-                calendar.add(Calendar.MONTH, -1);
-            }
-
-            updateDate();
-
-        });
-
-        binding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                if (tab.getText().equals("Monthly")) {
-                    Constants.SELECTED_TAB_STATS = 1;
-                    updateDate();
-
-                } else if (tab.getText().equals("Daily")) {
-                    Constants.SELECTED_TAB_STATS= 0;
-                    updateDate();
-
-                }
-
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-
-        Pie pie = AnyChart.pie();
-
-
-        viewModel.categoriesTransactions.observe(getViewLifecycleOwner(), new Observer<RealmResults<Transaction>>()  {
-            @Override
-            public void onChanged(RealmResults<Transaction> transactions) {
-                List<DataEntry> data = new ArrayList<>();
-
-                if (!transactions.isEmpty()) {
-                    Map<String, Double> categoryMap = new HashMap<>();
-
-                    for (Transaction transaction : transactions)
-                    {
-                        String category = transaction.getCategory();
-                        double amount = transaction.getAmount();
-
-                        if(categoryMap.containsKey(category)) {
-                            double currentTotal = categoryMap.get(category).doubleValue();
-                            currentTotal += Math.abs(amount);
-
-                            categoryMap.put(category, currentTotal);
-                        }
-                        else {
-                            categoryMap.put(category, Math.abs(amount));
-                        }
-
-                    }
-
-                    for(Map.Entry<String, Double> entry : categoryMap.entrySet()) {
-
-                        data.add(new ValueDataEntry(entry.getKey(),entry.getValue()));
-                    }
-
-                    pie.data(data);
-                }
-
-            }
-
-        });
-
-        viewModel.getTransactions(calendar,  SELECTED_STATS_TYPE);
-
-        binding.anyChart.setChart(pie);
-
-
-
-        return binding.getRoot();
-    }
-
-    void updateDate() {
-        if (Constants.SELECTED_TAB_STATS == Constants.DAILY) {
-            binding.currentDate.setText(Helper.formatDate(calendar.getTime()));
-
-        } else if (Constants.SELECTED_TAB_STATS == Constants.MONTHLY) {
-            binding.currentDate.setText(Helper.formatDateByMonth(calendar.getTime()));
+        public StatsFragment() {
+            // Required empty public constructor
         }
 
-        viewModel.getTransactions(calendar,  SELECTED_STATS_TYPE);
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        FragmentStatsBinding binding;
+        Calendar calendar;
+    /*
+    0 = Daily
+    1 = Monthly
+     */
+
+
+        public MainViewModel viewModel;
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            binding = FragmentStatsBinding.inflate(inflater);
+
+            viewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
+
+            calendar = Calendar.getInstance();
+            updateDate();
+
+            binding.incomeBtn.setOnClickListener(view -> {
+                binding.incomeBtn.setBackground(getContext().getDrawable(android.R.color.holo_green_dark));
+                binding.expenseBtn.setBackground(getContext().getDrawable(R.color.darkGrey));
+                binding.expenseBtn.setTextColor(getContext().getColor(R.color.textColor));
+                binding.incomeBtn.setTextColor(getContext().getColor(R.color.greenColor));
+
+                SELECTED_STATS_TYPE = Constants.INCOME;
+                updateDate();
+            });
+
+            binding.expenseBtn.setOnClickListener(view -> {
+                binding.incomeBtn.setBackground(getContext().getDrawable(R.color.darkGrey));
+                binding.expenseBtn.setBackground(getContext().getDrawable(android.R.color.holo_red_dark));
+                binding.incomeBtn.setTextColor(getContext().getColor(R.color.textColor));
+                binding.expenseBtn.setTextColor(getContext().getColor(R.color.redColor));
+
+                SELECTED_STATS_TYPE = Constants.EXPENSE;
+                updateDate();
+            });
+
+            binding.nextDate.setOnClickListener(c-> {
+                if(Constants.SELECTED_TAB_STATS == Constants.DAILY) {
+                    calendar.add(Calendar.DATE, 1);
+                } else if(Constants.SELECTED_TAB_STATS == Constants.MONTHLY) {
+                    calendar.add(Calendar.MONTH, 1);
+                }
+                updateDate();
+            });
+
+            binding.prevDate.setOnClickListener(c-> {
+                if(Constants.SELECTED_TAB_STATS == Constants.DAILY) {
+                    calendar.add(Calendar.DATE, -1);
+                } else if(Constants.SELECTED_TAB_STATS == Constants.MONTHLY) {
+                    calendar.add(Calendar.MONTH, -1);
+                }
+                updateDate();
+            });
+
+            binding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                @Override
+                public void onTabSelected(TabLayout.Tab tab) {
+                    if(tab.getText().equals("Monthly")) {
+                        Constants.SELECTED_TAB_STATS = 1;
+                        updateDate();
+                    } else if(tab.getText().equals("Daily")) {
+                        Constants.SELECTED_TAB_STATS = 0;
+                        updateDate();
+                    }
+                }
+
+                @Override
+                public void onTabUnselected(TabLayout.Tab tab) {
+
+                }
+
+                @Override
+                public void onTabReselected(TabLayout.Tab tab) {
+
+                }
+            });
+
+
+            Pie pie = AnyChart.pie();
+
+            viewModel.categoriesTransactions.observe(getViewLifecycleOwner(), new Observer<RealmResults<Transaction>>() {
+                @Override
+                public void onChanged(RealmResults<Transaction> transactions) {
+
+
+                    if(transactions.size() > 0) {
+
+                        binding.emptyState.setVisibility(View.GONE);
+                        binding.anyChart.setVisibility(View.VISIBLE);
+
+                        List<DataEntry> data = new ArrayList<>();
+
+                        Map<String, Double> categoryMap = new HashMap<>();
+
+                        for(Transaction transaction : transactions) {
+                            String category = transaction.getCategory();
+                            double amount = transaction.getAmount();
+
+                            if(categoryMap.containsKey(category)) {
+                                double currentTotal = categoryMap.get(category).doubleValue();
+                                currentTotal += Math.abs(amount);
+
+                                categoryMap.put(category, currentTotal);
+                            } else {
+                                categoryMap.put(category, Math.abs(amount));
+                            }
+                        }
+
+                        for(Map.Entry<String, Double> entry : categoryMap.entrySet()) {
+                            data.add(new ValueDataEntry(entry.getKey(),entry.getValue()));
+                        }
+                        pie.data(data);
+
+                    } else {
+                        binding.emptyState.setVisibility(View.VISIBLE);
+                        binding.anyChart.setVisibility(View.GONE);
+                    }
+
+
+                }
+            });
+
+            viewModel.getTransactions(calendar, SELECTED_STATS_TYPE);
+
+
+
+//
+//        pie.title("Fruits imported in 2015 (in kg)");
+//
+//        pie.labels().position("outside");
+//
+//        pie.legend().title().enabled(true);
+//        pie.legend().title()
+//                .text("Retail channels")
+//                .padding(0d, 0d, 10d, 0d);
+//
+//        pie.legend()
+//                .position("center-bottom")
+//                .itemsLayout(LegendLayout.HORIZONTAL)
+//                .align(Align.CENTER);
+
+            binding.anyChart.setChart(pie);
+
+
+            return binding.getRoot();
+        }
+
+        void updateDate() {
+            if(Constants.SELECTED_TAB_STATS == Constants.DAILY) {
+                binding.currentDate.setText(Helper.formatDate(calendar.getTime()));
+            } else if(Constants.SELECTED_TAB_STATS == Constants.MONTHLY) {
+                binding.currentDate.setText(Helper.formatDateByMonth(calendar.getTime()));
+            }
+            viewModel.getTransactions(calendar, SELECTED_STATS_TYPE);
+        }
     }
-}
